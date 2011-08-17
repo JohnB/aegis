8a43f609c99cabfdd57bc8a172ff57346c0c0aea
Meredith Lesly <bitcababy+github@gmail.com>
2011-08-17 10:54:33 -0400

    Bleh. replace after_initialize with
    set_callback :initialize, :after, :set_default_role_name

--- lib/aegis/has_role.rb
+++ lib/aegis/has_role.rb
@@ -55,7 +55,7 @@ module Aegis
           end
         end
 
-        ActiveRecord::Base.after_initialize :set_default_role_name
+        set :initialize, :after, :set_default_role_name
 
       end
 
